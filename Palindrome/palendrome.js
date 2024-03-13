79aa9bd12e275ebb12690d80bb66a9c773b7e36f
butterfly1of4 <jhouck29@gmail.com>
2024-03-13 13:45:13 -0400

    made updates added button functionality

--- Palindrome/palendrome.js
+++ Palindrome/palendrome.js
@@ -1,13 +1,13 @@
 function palindrome(str) {
-  var x;
-  var compare_array = [];
-  var forward_loop = [];
-  var backward_loop = [];
-  var str = str.replace(/\W/g, "").split("_").pop() //takes out symbols, spaces and special characters
-  var forward = str.toLowerCase(); //ignore case
+  let x;
+  let compare_array = [];
+  let forward_loop = [];
+  let backward_loop = [];
+  let str = str.replace(/\W/g, "").split("_").pop() //takes out symbols, spaces and special characters
+  let forward = str.toLowerCase(); //ignore case
   let forward_array = forward.split(""); //makes an array from forward letters
   let backward_array = reverseString(forward); //reverses 'forward' into array
-  var backward = backward_array.join(""); //joins the backward arr into a string
+  let backward = backward_array.join(""); //joins the backward arr into a string
 
   for (let x = 0; x < forward_array.length; x++) {
     forward_loop.push(forward_array[x]);
